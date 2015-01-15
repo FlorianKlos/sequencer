ecd048ce6d143539b12d3452cde7aa4110e39c50
Florian Klos <florian.klos91@gmail.com>
2015-01-15 10:41:14 +0100

    add scripts to index.html and start application in app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,54 @@
-(function () {
+(function (AudioContext, Sequencer) {
     'use strict';
-})();
+
+    if (typeof AudioContext === 'undefined' ||typeof Sequencer === 'undefined') {
+        return;
+    }
+
+    let sequencerContainer = document.getElementsByClassName('sequencer')[0];
+    let errorMessage = document.getElementsByClassName('error-message')[0];
+    sequencerContainer.removeChild(errorMessage);
+
+    let sounds = [
+        {
+            name: 'ClHat',
+            file: './sounds/drumkit/CYCdh_ElecK01-ClHat01.wav'
+        },
+        {
+            name: 'OpHat01',
+            file: './sounds/drumkit/CYCdh_ElecK01-OpHat01.wav'
+        },
+        {
+            name: 'Cymbal',
+            file: './sounds/drumkit/CYCdh_ElecK01-Cymbal.wav'
+        },
+        {
+            name: 'Kick01',
+            file: './sounds/drumkit/CYCdh_ElecK01-Kick01.wav'
+        },
+        {
+            name: 'Snr01',
+            file: './sounds/drumkit/CYCdh_ElecK01-Snr01.wav'
+        },
+        {
+            name: 'Tom01',
+            file: './sounds/drumkit/CYCdh_ElecK01-Tom01.wav'
+        },
+        {
+            name: 'Tom02',
+            file: './sounds/drumkit/CYCdh_ElecK01-Tom02.wav'
+        },
+        {
+            name: 'Tom03',
+            file: './sounds/drumkit/CYCdh_ElecK01-Tom03.wav'
+        },
+        {
+            name: 'Tom04',
+            file: './sounds/drumkit/CYCdh_ElecK01-Tom04.wav'
+        }
+    ];
+    let bars = 4;
+    let bpm = 75;
+
+    let sequencer = new Sequencer(sounds, bars, bpm, sequencerContainer);
+})(window.AudioContext, window.Sequencer);
